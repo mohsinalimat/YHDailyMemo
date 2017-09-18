793cd1aa10d81f4e61fc7a5643e359bdf341920b
Yohan Hyunsung Yi <hyunsungyi@gmail.com>
2017-09-18 00:42:00 -0700

    update version 1.3

--- SimpleCalendar/AppDelegate.swift
+++ SimpleCalendar/AppDelegate.swift
@@ -77,19 +77,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
         
-        //Return to App
-        if self.lock == true {
-            let lockViewController = storyboard.instantiateViewController(withIdentifier: "PasswordLoginViewController") as! PasswordLoginViewController
-            self.window?.rootViewController = lockViewController
-        } else {
-            let viewController = storyboard.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
-            self.window?.rootViewController = viewController
-        }
-        
-        self.window?.makeKeyAndVisible()
     
     }
 
